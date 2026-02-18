b41c2d60a362c6b6156ff8abd19f3e386ae1f119
Pawel Znoj <pepe.znoj@gmail.com>
2026-02-18 15:47:56 +0100

    Initial Spring Boot project setup

---
+++ src/main/java/pl/pezet/user_form_app/UserFormAppApplication.java
@@ -0,0 +1,13 @@
+package pl.pezet.user_form_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserFormAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserFormAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pezet/user_form_app/UserFormAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pezet.user_form_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserFormAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
