51d1be70ba327975bc49e9d9c85ae7cdd3846df7
Pawel Znoj <pepe.znoj@gmail.com>
2026-02-19 20:09:55 +0100

    Dodanie walidacji pól na formularzu

--- src/main/java/pl/pezet/user_form_app/model/User.java
+++ src/main/java/pl/pezet/user_form_app/model/User.java
@@ -1,6 +1,10 @@
 package pl.pezet.user_form_app.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Entity
@@ -14,8 +18,17 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Imię jest wymagane")
     private String firstName;
+
+    @NotBlank(message = "Nazwisko jest wymagane")
     private String lastName;
+
+    @NotNull(message = "Wiek jest wymagany")
+    @Min(value = 0, message = "Wiek musi być większy lub równy 0")
     private Integer age;
+
+    @NotBlank(message = "Email jest wymagany")
+    @Email(message = "Niepoprawny format email")
     private String email;
 }
